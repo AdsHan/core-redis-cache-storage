deff2f695514aac13506c813086ee8fc3bddc08d
Anderson Hansen <adshan@gmail.com>
2021-07-29 18:21:02 -0300

    Ajustes na documentação do Swagger

--- src/services/CDI.Catalog.API/Configuration/SwaggerConfig.cs
+++ src/services/CDI.Catalog.API/Configuration/SwaggerConfig.cs
@@ -15,37 +15,12 @@ namespace CDI.Catalog.API.Configuration
             {
                 c.SwaggerDoc("v1", new OpenApiInfo()
                 {
-                    Title = "JWT Refresh Token",
-                    Description = "API desenvolvida para demonstração de JWT com Refresh Tokens",
+                    Title = "Cache Distribuído",
+                    Description = "API desenvolvida para demonstração de cache distribuído com Redis",
                     Contact = new OpenApiContact() { Name = "Anderson Hansen", Email = "adshan@gmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
                 });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-                    Name = "Authorization",
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-                    }
-                });
-
+              
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
